1a6c60a7e1169204f889214f50aa3baeaacc1128
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-06-27 11:48:19 +0530

    second heading

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 const heading = React.createElement("h1",{}, "Hello World From React World!");
+const heading2 = React.createElement("h1",{},"React tutorial");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+root.render(heading2);
