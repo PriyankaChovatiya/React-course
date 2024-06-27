e8ba0d7c1ea6a7623e9fa8fc2a790cd5f6a4f98d
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-06-27 13:16:22 +0530

    two child

--- App.js
+++ App.js
@@ -11,8 +11,18 @@
 */
 const parent = React.createElement
   ("div",{id:"parent"},
- React.createElement("div",{id:"child"}, 
-    [React.createElement("h1",{}, "I'm h1 tag"), React.createElement("h2",{}, "I'm h2 tag")]) );
+ [React.createElement("div",{id:"child"}, 
+    [React.createElement("h1",{}, "I'm h1 tag"), 
+    React.createElement("h2",{}, "I'm h2 tag")])],
+    [
+    React.createElement("div",{id:"child2"}, 
+    [React.createElement("h1",{}, "I'm h1 tag"), 
+    React.createElement("h2",{}, "I'm h2 tag")])
+
+    ]
+
+
+);
 
 
 
