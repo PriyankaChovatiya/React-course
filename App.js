8f65ab51eef0762d7b090479e98d184957e75a79
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 11:55:55 +0530

    Single line JSX Syntax

--- App.js
+++ App.js
@@ -5,8 +5,15 @@ import ReactDOM from 'react-dom';
 
 const heading  = React.createElement("h1", {id:"heading"} , "Hello Form React World" );
 
+//JSX - (Transpiled before it reaches the JS) - PARCEL - BABEL
+//JSX => React.createElement =>React Element => Js object => HTMLElement
 
+//Single line 
+const jsxheading  = <h1>Welcome to the React Course</h1>;
+
+
+// create Root 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // render the root
-root.render(heading);
+root.render(jsxheading);
