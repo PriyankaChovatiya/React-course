d4633bb40d7c1caf23eed6b1c44708fab0553e36
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 12:37:02 +0530

    3 ways to write Functional Components

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// core React - create the element
+//Java script functional component
+//const App = () => {   }
 
-const heading  = React.createElement("h1", {id:"heading"} , "Hello Form React World" );
 
-//JSX - (Transpiled before it reaches the JS) - PARCEL - BABEL
-//JSX => Babel Transplied it to React.createElement =>React Element => Js object => HTMLElement(render)
+//Two types of component - capital letter
+//Class component 
 
-//Single line JSX
-const jsxheading  = <h1 id = "heading">Welcome to the React Course</h1>;
 
-//Multi line JSX
-const jsxheading2 = (
-  <h1 id ="heading">
+//Functional component
+//1st way
+const jsxheading1  = () => {
+  return <h1 id = "heading">Welcome to the React Course</h1>
+};
+
+//2nd way - but some time developer are not write Return 
+const jsxheading2 = () => {(
+  <h1 id ="heading" className="heading">
     Welcome To React Class
   </h1>
-);
+)};
 
-//Add the Attributes in element in camelCase  -  class => className
-const jsxheading3 = (
-  <h1 id="heading" className="heading3">Hello</h1>
-);
+//3rd way
+const jsxheading3 = () => (<h1>Hello</h1>);
 
 
 // create Root 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // render the root
-root.render(jsxheading2);
+root.render(jsxheading1);
