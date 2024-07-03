0e2fc3d8f1bedc8921ceb9431a385c3661254b1f
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 16:01:17 +0530

    calling title using function {title()}

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const head = <span>React Message</span>
 
-const title = (<h1 id="heading" className="heading">{head}Hello Priyaa!</h1> );
+const title = () => (<h1 id="heading" className="heading">Hello Priyaa!</h1> );
 
 //Component Composition
 const HeadingComponent = () => (
 <div id=" Container">
-  {title}
+  {title()}
   <h1 id = "heading" className = "heading">
     Welcome To React Class
   </h1>
