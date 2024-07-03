69d3b4813bc39563223e7cb354fe52e2dddc59d1
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 11:58:18 +0530

    multi line JSX Syntax

--- App.js
+++ App.js
@@ -8,12 +8,19 @@ const heading  = React.createElement("h1", {id:"heading"} , "Hello Form React Wo
 //JSX - (Transpiled before it reaches the JS) - PARCEL - BABEL
 //JSX => React.createElement =>React Element => Js object => HTMLElement
 
-//Single line 
-const jsxheading  = <h1>Welcome to the React Course</h1>;
+//Single line JSX
+const jsxheading  = <h1 id = "heading">Welcome to the React Course</h1>;
+
+//Multi line JSX
+const jsxheading2 = (
+<h1 id ="heading">
+   Welcome To React Class
+</h1>
+);
 
 
 // create Root 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // render the root
-root.render(jsxheading);
+root.render(jsxheading2);
