97b08caa359fef6f50ca128708ebec7852c7e691
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 12:42:46 +0530

    Making nested Elements

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Java script functional component
-//const App = () => {   }
 
-
-//Two types of component - capital letter
-//Class component 
-
+//React Element 
+const heading = React.createElement("h1" , {id:"heading" , className:"heading"}, " hello ");
 
 //Functional component
-//1st way
-const jsxheading1  = () => {
-  return <h1 id = "heading">Welcome to the React Course</h1>
-};
-
-//2nd way - but some time developer are not write Return 
-const jsxheading2 = () => {(
-  <h1 id ="heading" className="heading">
+const HeadingCompont = () => (
+<div id=" Container">
+  <h1 id = "heading" className = "heading">
     Welcome To React Class
   </h1>
-)};
-
-//3rd way
-const jsxheading3 = () => (<h1>Hello</h1>);
-
+  </div>
+);
 
 // create Root 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // render the root
-root.render(jsxheading1);
+root.render(heading);
