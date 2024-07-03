4472906ab1d1c38b14cd642d1cd9bb3b66a6f285
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 15:44:42 +0530

    React-element inside the React component

--- App.js
+++ App.js
@@ -2,31 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-const Title = () => <h1 id="heading" className="heading">Hello Priyaa!</h1>
+const title = <h1 id="heading" className="heading">Hello Priyaa!</h1>
 
 //Component Composition
 const HeadingComponent = () => (
 <div id=" Container">
+  {title}
   <h1 id = "heading" className = "heading">
     Welcome To React Class
   </h1>
   </div>
 );
 
-const Display = () =>
-  <div id= "Container2">
-    <HeadingComponent/>
-    <Title/>
-       <p>  
-        Hello, I'm Priyaa Today I'm Explaining the Component Composition. Component Composition : It means you are calling one components in another copmponent.
-        </p>
-  </div>
-
-
-// create Root 
+//create Root 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //render component
-root.render(<Display/>);
-
+root.render(<HeadingComponent/>);
