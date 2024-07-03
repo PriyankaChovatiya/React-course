28cc46b6d9f1b2ba05b89c33002c63a9d9765b16
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-03 13:05:05 +0530

    Component Composition

--- App.js
+++ App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-//React Element 
-const heading = <h1 id="heading" className="heading"></h1>
 
-//Functional component
+const Title = () => <h1 id="heading" className="heading">Hello Priyaa!</h1>
+
+//Component Composition
 const HeadingComponent = () => (
 <div id=" Container">
   <h1 id = "heading" className = "heading">
     Welcome To React Class
   </h1>
+  <Title/> 
   </div>
 );
 
 // create Root 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// render an element
-root.render(heading);
 
 //render component
 root.render(<HeadingComponent/>);
+
